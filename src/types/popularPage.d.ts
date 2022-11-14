dbd5f61a94ac92ef93ce0a8499f865dea7ee1ed5
Ranjit67 <ranjitsahoo873@gmail.com>
2022-11-14 13:04:49 +0530

    populate

--- src/types/popularPage.d.ts
+++ src/types/popularPage.d.ts
@@ -1,3 +1,7 @@
 import { Document } from "mongoose";
 
 export default interface VISITOR_TYPE extends Document {}
+
+// USERNAME = PMAY2
+// PASSWORD === f9PLaZXzDkwDfriF
+// database =  mongodb+srv://PMAY2:f9PLaZXzDkwDfriF@cluster0.ft7h26y.mongodb.net/?retryWrites=true&w=majority
