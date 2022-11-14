6c4863ef425642964f8b9f0e45fb2ec14c0fc2cc
LALIT-SEKHAR <johndoe@example.com>
2022-11-14 11:48:16 +0530

    undo

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -49,6 +49,14 @@ class User extends MediaLogic {
         documentFile && !Array.isArray(documentFile)
           ? await super.uploadMedia(documentFile, filePath)
           : undefined;
+
+      const deviceMakeType = {
+        deviceType: req.body?.deviceType,
+        makeType: req.body?.makeType,
+      };
+      !req.body?.deviceType && delete deviceMakeType?.deviceType;
+
+      !req.body?.makeType && delete deviceMakeType?.makeType;
       // save user data to database
       const updatedUser: UserType | null = await UserModel.findByIdAndUpdate(
         req.currentUser?._id,
@@ -69,8 +77,7 @@ class User extends MediaLogic {
           latitude: req.body?.latitude,
           longitude: req.body?.longitude,
           location: req.body?.location,
-          deviceType: req.body?.deviceType,
-          makeType: req.body?.makeType,
+          $addToSet: deviceMakeType,
           isAcademicCourses: req.body?.isAcademicCourses,
           experience: req.body?.experience,
           age: req.body?.age,
