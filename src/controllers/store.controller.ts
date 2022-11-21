cf846fe69c47cb33db11d68f87959a50a9e9cb42
Ranjit67 <ranjitsahoo873@gmail.com>
2022-11-21 11:46:23 +0530

    bug fix

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Response } from "express";
-import { body, oneOf, query } from "express-validator";
+import { body, query } from "express-validator";
 import { Types } from "mongoose";
 import { fieldValidateError } from "../helper";
 import MediaLogic from "../logic/media.logic";
@@ -1004,21 +1004,17 @@ export const storeControlValidator = {
       .isMongoId()
       .withMessage("serviceId must be mongoose id."),
 
-    oneOf([
-      query("modelId.*")
-        .not()
-        .isEmpty()
-        .withMessage("modelId is required.")
-        .isMongoId()
-        .withMessage("modelId must be mongoose id."),
+    query("modelId.*")
+      .not()
+      .isEmpty()
+      .withMessage("modelId is required.")
+      .isMongoId()
+      .withMessage("modelId must be mongoose id."),
 
-      query("modelId")
-        .not()
-        .isEmpty()
-        .withMessage("modelId is required.")
-        .isMongoId()
-        .withMessage("modelId must be mongoose id."),
-    ]),
+    query("modelId")
+      .optional()
+      .isMongoId()
+      .withMessage("modelId must be mongoose id."),
   ],
 };
 
