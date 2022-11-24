0cfb1be19848abbaef9963805f9eb20838b34e17
@ranjitsahoo873 <ranjitsahoo873@gmail.com>
2022-11-24 13:58:35 +0530

    bank update

--- src/models/bank.model.ts
+++ src/models/bank.model.ts
@@ -3,11 +3,24 @@ import BankType from "../types/bank";
 
 const bankSchema = new Schema<BankType, Model<BankType>>(
   {
-    fullName: String,
-    accountNumber: Number,
-    bankName: String,
-    SORDCode: String,
+    fullName: {
+      type: String,
+      required: [true, "fullName is required."],
+    },
+    accountNumber: {
+      type: Number,
+      required: [true, "accountNumber is required."],
+    },
+    bankName: {
+      type: String,
+      required: [true, "bankName is required."],
+    },
+    SORDCode: {
+      type: String,
+      required: [true, "SORDCode is required."],
+    },
     user: {
+      required: [true, "user is required."],
       type: Schema.Types.ObjectId,
       ref: "User",
     },
