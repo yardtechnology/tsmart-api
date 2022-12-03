db33e72a634eaa9c7e8e1f9596beb905ea0e0af6
Ranjit67 <ranjitsahoo873@gmail.com>
2022-12-03 15:26:41 +0530

    check

--- build/models/faq.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FAQModel = void 0;
-var mongoose_1 = require("mongoose");
-var faqSchema = new mongoose_1.Schema({
-    question: String,
-    answer: String,
-}, { timestamps: true });
-exports.FAQModel = (0, mongoose_1.model)("Faq", faqSchema);
