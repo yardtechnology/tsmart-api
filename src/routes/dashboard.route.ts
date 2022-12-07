58408bb3c7747cc683bba2b50bcccac37d3883af
Ranjit67 <ranjitsahoo873@gmail.com>
2022-12-07 10:13:50 +0530

    comment remove

--- src/routes/dashboard.route.ts
+++ src/routes/dashboard.route.ts
@@ -45,48 +45,6 @@ export default class DashboardRoutes extends AuthenticateMiddleware {
     this.routes();
   }
   private routes() {
-    // create
-    // this.router.get(
-    //   "/dashboard/order-status",
-    //   super.isAuthenticated,
-    //   DashboardControllerValidation.orderStatusAndServiceType,
-    //   this.dashboardController.orderStatusAndServiceType
-    // );
-
-    // // total user count
-    // this.router.get(
-    //   "/dashboard/user-count",
-    //   super.isAuthenticated,
-    //   DashboardControllerValidation.totalUserCount,
-    //   this.dashboardController.totalUserCount
-    // );
-
-    // //  Repair count
-    // this.router.get(
-    //   "/dashboard/repair-order-count",
-    //   super.isAuthenticated,
-    //   this.dashboardController.repairOrderCount
-    // );
-    // // Refurbished count
-    // this.router.get(
-    //   "/dashboard/refurbish-and-accessory-product-count",
-    //   super.isAuthenticated,
-    //   DashboardControllerValidation.refurbishedProductCount,
-    //   this.dashboardController.refurbishedProductCount
-    // );
-    // // Revenue generate
-    // this.router.get(
-    //   "/dashboard/revenue",
-    //   super.isAuthenticated,
-    //   this.dashboardController.revenue
-    // );
-    // // Store Count
-    // this.router.get(
-    //   "/dashboard/store-count",
-    //   super.isAuthenticated,
-    //   this.dashboardController.storeCount
-    // );
-
     // ================================================================= >>>> USER DASHBOARD <<<< =================================================================
     this.router.get(
       "/dashboard/user/weekly-join",
