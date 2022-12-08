d6f79fafc73c7f06f8f35ae20c1c3f6d070da7bf
LALIT-SEKHAR <johndoe@example.com>
2022-12-08 17:34:59 +0530

    map socket add

--- src/socket/position.socket.ts
+++ src/socket/position.socket.ts
@@ -1,18 +1,23 @@
-export default (Io: any) => async (socket: any) => {
+export default (Io: any) => (socket: any) => {
   socket.on(
     "ENTER-POSITION-CHANGE-ROOM",
-    async (payload: { technicianId: string }) => {
+    (payload: { technicianId: string }) => {
       console.log("ENTERED-POSITION-CHANGE-ROOM", payload);
       socket.join(`POSITION-CHANGE-ROOM-${payload?.technicianId?.toString()}`);
     }
   );
   socket.on(
     "POSITION-CHANGE-ROOM-DATA-CHANGE",
-    async (payload: { technicianId: string }) => {
+    (payload: {
+      technicianId: string;
+      lat: number;
+      lng: number;
+      heading: number;
+    }) => {
       console.log("POSITION-CHANGE-ROOM-DATA-CHANGE", payload);
-      socket.join(
-        `POSITION-CHANGE-ROOM-${payload?.technicianId?.toString()}-DATA-CHANGE`
-      );
+      Io.of("/position")
+        .to(`POSITION-CHANGE-ROOM-${payload?.technicianId?.toString()}`)
+        .emit("POSITION-CHANGE-ROOM-DATA-CHANGE", payload);
     }
   );
 };
