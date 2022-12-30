24b66f18cc07ec8ef240b8868fe89b9a7cad50e9
Ranjit67 <ranjitsahoo873@gmail.com>
2022-12-30 16:35:56 +0530

    build file delete

--- build/models/color.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var colorSchema = new mongoose_1.Schema({
-    color: String,
-    hashCode: String,
-}, { timestamps: true });
-var ColorSchema = (0, mongoose_1.model)("Color", colorSchema);
-exports.default = ColorSchema;
--- build/models/memory.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var memorySchema = new mongoose_1.Schema({
-    internal: String,
-    ram: String,
-}, { timestamps: true });
-var MemorySchema = (0, mongoose_1.model)("Memory", memorySchema);
-exports.default = MemorySchema;
